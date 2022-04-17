59d1766c8db148a3b1dd47a5d6188a395d3c0981
Salah Chadli <salah.chadli@gmail.com>
2022-04-17 13:44:22 +0200

    add a node for data ingestion

---
+++ servier/nodes/data_ingestion.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import pandas as pd
+from ..constants import logging
+from pathlib import Path
+
+
+def ingest_data(data_path, msg_type=None, ):
+    if msg_type is not None:
+        msg_type = f"| {msg_type} "
+    else:
+        msg_type = ""
+    logging.info(f" Data Ingestion {msg_type}| Start!")
+    if type(data_path) == str:
+        data_path = Path(data_path)
+    df = pd.read_csv(data_path)
+    logging.info(f" Data Ingestion {msg_type}| {len(df)} rows ingested")
+    logging.info(f" Data Ingestion {msg_type}| Finished!")
+    return df
+
