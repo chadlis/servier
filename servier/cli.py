8f169bb4a55ad5776de75831b59ee1fd781ac9fa
Salah Chadli <salah.chadli@gmail.com>
2022-04-17 14:07:32 +0200

    add a splitting pipeline

--- servier/cli.py
+++ servier/cli.py
@@ -2,24 +2,44 @@
 import argparse
 import sys
 
-from .config import (DATA_PATH, RAW_DATA_PATH, PRIMARY_DATA_PATH, MODEL_DATA_PATH, REPORTING_DATA_PATH)
-from .config import (INPUT_FILENAME, DATA_TRAIN_FILENAME, DATA_VALID_FILENAME, DATA_TEST_FILENAME, MODEL_DATA_PATH_NAME)
+from .config import (
+    RAW_DATA_PATH,
+    PRIMARY_DATA_PATH,
+    MODEL_DATA_PATH,
+    REPORTING_DATA_PATH,
+)
+from .config import (
+    INPUT_FILENAME,
+    DATA_TRAIN_FILENAME,
+    DATA_VALID_FILENAME,
+    DATA_TEST_FILENAME,
+    MODEL_DATA_PATH_NAME,
+)
+
+from .pipelines.data_splitting import split_data
+
 
 def main():
     """Console script for servier."""
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('_',
-    default='train',
-    choices=["split", 'train', 'evaluate', 'predict'],
-    help='split data, train, evaluate or predict (default: %(default)s)')
+    parser.add_argument(
+        "_",
+        default="train",
+        choices=["split", "train", "evaluate", "predict"],
+        help="split data, train, evaluate or predict (default: %(default)s)",
+    )
 
-    parser.add_argument("--input_path", type=str, help="Input data path",)
+    parser.add_argument(
+        "--input_path",
+        type=str,
+        help="Input data path",
+    )
     parser.add_argument("--valid_path", type=str, help="Validation data path")
     parser.add_argument("--output_path", type=str, help="Output path")
     parser.add_argument("--model_path", type=str, help="Model path")
     parser.add_argument("--reporting_path", type=str, help="Reporting path")
-    parser.add_argument("--test_only", action='store_true', help="Output path")
+    parser.add_argument("--test_only", action="store_true", help="Output path")
 
     args = parser.parse_args()
     mode = args._
@@ -32,44 +52,43 @@ def main():
 
     if mode == "split":
         if input_path is None:
-            input_path = RAW_DATA_PATH/INPUT_FILENAME
+            input_path = RAW_DATA_PATH / INPUT_FILENAME
         if output_path is None:
             output_path = PRIMARY_DATA_PATH
         split_data(input_path, output_path, test_only=test_only)
     elif mode == "train":
         if input_path is None:
-            input_path = PRIMARY_DATA_PATH/DATA_TRAIN_FILENAME
+            input_path = PRIMARY_DATA_PATH / DATA_TRAIN_FILENAME
             if valid_path is None:
-                valid_path = PRIMARY_DATA_PATH/DATA_VALID_FILENAME   
+                valid_path = PRIMARY_DATA_PATH / DATA_VALID_FILENAME
         if model_path is None:
-            model_path = MODEL_DATA_PATH/MODEL_DATA_PATH_NAME
+            model_path = MODEL_DATA_PATH / MODEL_DATA_PATH_NAME
         if reporting_path is None:
-            reporting_path = REPORTING_DATA_PATH 
-        train(input_path, valid_path=valid_path, model_path=model_path, reporting_path=reporting_path)
+            reporting_path = REPORTING_DATA_PATH
+        train(
+            input_path,
+            valid_path=valid_path,
+            model_path=model_path,
+            reporting_path=reporting_path,
+        )
     elif mode == "evaluate":
         if input_path is None:
-            input_path = PRIMARY_DATA_PATH/DATA_TEST_FILENAME
+            input_path = PRIMARY_DATA_PATH / DATA_TEST_FILENAME
         if model_path is None:
-            model_path = MODEL_DATA_PATH/MODEL_DATA_PATH_NAME
+            model_path = MODEL_DATA_PATH / MODEL_DATA_PATH_NAME
         if reporting_path is None:
-            reporting_path = REPORTING_DATA_PATH 
+            reporting_path = REPORTING_DATA_PATH
         evaluate(input_path, model_path, reporting_path)
     elif mode == "predict":
         if input_path is None:
-            input_path = PRIMARY_DATA_PATH/DATA_TEST_FILENAME
+            input_path = PRIMARY_DATA_PATH / DATA_TEST_FILENAME
         if model_path is None:
-            model_path = MODEL_DATA_PATH/MODEL_DATA_PATH_NAME
+            model_path = MODEL_DATA_PATH / MODEL_DATA_PATH_NAME
         if reporting_path is None:
             reporting_path = REPORTING_DATA_PATH
         predict(input_path, model_path, reporting_path)
     return 0
 
-
-def split_data(input_path, output_path, test_only):
-    print("Split..")
-    print(f"input_path: {input_path}")
-    print(f"output_path: {output_path}")
-    print(f"test_only: {test_only}")
 
 def train(input_path, valid_path, model_path, reporting_path):
     print("Train..")
@@ -78,17 +97,20 @@ def train(input_path, valid_path, model_path, reporting_path):
     print(f"model_path: {model_path}")
     print(f"reporting_path: {reporting_path}")
 
+
 def evaluate(input_path, model_path, reporting_path):
     print("Evaluate..")
     print(f"input_path: {input_path}")
     print(f"model_path: {model_path}")
     print(f"reporting_path: {reporting_path}")
 
+
 def predict(input_path, model_path, reporting_path):
     print("Predict..")
     print(f"input_path: {input_path}")
     print(f"model_path: {model_path}")
     print(f"reporting_path: {reporting_path}")
+
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
--- servier/config.py
+++ servier/config.py
@@ -5,17 +5,18 @@ from pandera import Column, DataFrameSchema, Check, Index
 
 logging.getLogger().setLevel(logging.DEBUG)
 
+RANDOM_STATE = 42
 
 COL_TARGET = "P1"
 COL_SMILES = "smiles"
 COL_ID = "mol_id"
 
-DATA_PATH = Path("data")
-RAW_DATA_PATH = DATA_PATH/"0_raw"
-PRIMARY_DATA_PATH = DATA_PATH/"1_primary"
-FEATURE_DATA_PATH = DATA_PATH/"2_feature"
-MODEL_DATA_PATH = DATA_PATH/"3_model"
-REPORTING_DATA_PATH = DATA_PATH/"4_reporting"
+DATA_PATH = Path("./../data")
+RAW_DATA_PATH = DATA_PATH / "0_raw"
+PRIMARY_DATA_PATH = DATA_PATH / "1_primary"
+FEATURE_DATA_PATH = DATA_PATH / "2_feature"
+MODEL_DATA_PATH = DATA_PATH / "3_model"
+REPORTING_DATA_PATH = DATA_PATH / "4_reporting"
 
 INPUT_FILENAME = "data.csv"
 
@@ -25,43 +26,59 @@ DATA_TRAIN_FILENAME = "data_train.csv"
 DATA_VALID_FILENAME = "data_valid.csv"
 DATA_TEST_FILENAME = "data_test.csv"
 
-TRAINING_SIZE = 0.15
+TRAINING_SIZE = 0.7
 MAX_EPOCHS = 2
 
 
-
-
-DATA_SCHEMA =  DataFrameSchema(
+DATA_SCHEMA = DataFrameSchema(
     {
-        COL_ID: Column(str,
-                        Check(lambda s: s.str.startswith("CID")),
-                        required=False,
-                        nullable=False,),
-        COL_SMILES: Column(str,
-                        Check(lambda s: len(s)>=1,),
-                        nullable=False,),
-        COL_TARGET: Column(int, Check.isin([0, 1]),
-                    nullable=False,),
+        COL_ID: Column(
+            str,
+            Check(lambda s: s.str.startswith("CID")),
+            required=False,
+            nullable=False,
+        ),
+        COL_SMILES: Column(
+            str,
+            Check(
+                lambda s: len(s) >= 1,
+            ),
+            nullable=False,
+        ),
+        COL_TARGET: Column(
+            int,
+            Check.isin([0, 1]),
+            nullable=False,
+        ),
     },
     index=Index(int),
-    strict='filter',
+    strict="filter",
     coerce=True,
 )
 
-DATA_SCHEMA_PREDICTION =  DataFrameSchema(
+DATA_SCHEMA_PREDICTION = DataFrameSchema(
     {
-        COL_ID: Column(str,
-                        Check(lambda s: s.str.startswith("CID")),
-                        required=False,
-                        nullable=False,),
-        COL_SMILES: Column(str,
-                        Check(lambda s: len(s)>=1,),
-                        nullable=False,),
-        COL_TARGET: Column(int, Check.isin([0, 1]),
-                     required=False,
-                     nullable=False,),
+        COL_ID: Column(
+            str,
+            Check(lambda s: s.str.startswith("CID")),
+            required=False,
+            nullable=False,
+        ),
+        COL_SMILES: Column(
+            str,
+            Check(
+                lambda s: len(s) >= 1,
+            ),
+            nullable=False,
+        ),
+        COL_TARGET: Column(
+            int,
+            Check.isin([0, 1]),
+            required=False,
+            nullable=False,
+        ),
     },
     index=Index(int),
-    strict='filter',
+    strict="filter",
     coerce=True,
 )
